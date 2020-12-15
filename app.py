daf15a69a73b92f20615677634021de4a5091916
MelissaMullenUNH <mdm1044@wildcats.unh.edu>
2020-12-15 17:19:45 -0500

    finalized code added comments neatened up

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+"""
+Sabra George and Melissa Mullen Group Implementation Project
+December 17, 2020
+COMP 740
+"""
+
 import os
-import json
 import base64
-import tensorflow as tf
+import random
+import cv2
 import googleapiclient.discovery
-from flask import Flask, render_template, request, jsonify
+import tensorflow as tf
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from googleapiclient import discovery
-from googleapiclient import errors
 from google.api_core.client_options import ClientOptions
 
 # Initialise Flask
@@ -18,9 +23,6 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'aikey.json'
 # Temporary storage for uploaded pictures
 UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Maximum Image Uploading size
-# app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 # Image extension allowed
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'bmp'])
@@ -37,6 +39,16 @@ def index():
 
 @app.route('/search', methods=['GET', 'POST'])
 def upload_file():
+    """
+    This function allows the user to upload a file.
+
+    Returns:
+        filepath: uploaded picture.
+        prediction_results: array of prediction results (floats)
+            defined by the model.
+        pred_look (float): probability of looking at screen.
+        pred_away (float): probability of looking away from screen.
+    """
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -58,16 +70,22 @@ def camera():
     """
     This function opens your webcam, takes a picture when you hit the space bar
     and sends the picture to the predictive model
+
+    Returns:
+        image: picture taken on your webcam (png file)
+        prediction_results: array of prediction results (floats)
+            defined by the model.
+        pred_look (float): probability of looking at screen.
+        pred_away (float): probability of looking away from screen.
     """
-    import cv2
-    import random
 
     camera = cv2.VideoCapture(0)
     cv2.namedWindow("Model Camera")
-    number = random.randint(0, 999)
+    number = random.randint(0, 999)   # for naming images
 
     while True:
         ret, frame = camera.read()
+        # if the camera cannot launch, return an error png
         if not ret:
             print("Failed to launch camera")
             image = "static/error.png"
@@ -76,22 +94,23 @@ def camera():
         cv2.imshow("Model Camera", frame)
 
         key = cv2.waitKey(1)
-        if key%256 == 27:
-            # ESC pressed
+        if key%256 == 27:   # ESC pressed
+            # If escape is pressed, close camera and return an error png
             print("Escape hit, closing camera")
             image = "static/error.png"
             break
 
         elif key%256 == 32:
             # SPACE pressed
+            # Take picture and save it
             image = "static/captured_image_{}.png".format(number)
             cv2.imwrite(image, frame)
             print("{} written!".format(image))
             print("Image captured, closing camera")
             break
 
-    camera.release()
-    cv2.destroyAllWindows()
+    camera.release()   # close camera
+    cv2.destroyAllWindows()   # destroy all windows the camera opened
 
     prediction_results, pred_look, pred_away = search(image)
     return render_template(
@@ -104,36 +123,35 @@ def camera():
 
 
 def search(f):
+    """
+    Send json data to a deployed model for prediction.
+    Args:
+        project (str): project where the Cloud ML Engine Model is deployed.
+        region (str): regional endpoint.
+        model (str): model name.
+        version (str): version of model.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of
+            datatypes convertible to tensors.
+    Returns:
+        prediction_results: array of prediction results (floats)
+            defined by the model.
+        pred_look (float): probability of looking at screen.
+        pred_away (float): probability of looking away from screen.
+    """
+
     with open(f, 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
     image_bytes = {'b64': str(encoded_string)}
     instances = [{'image_bytes': image_bytes, 'key': '1'}]
 
-    """
-    Send json data to a deployed model for prediction.
-    Args:
-        project (str): project where the Cloud ML Engine Model is deployed.
-        region (str): regional endpoint to use; set to None for
-        ml.googleapis.com
-        model (str): model name.
-        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
-            your deployed model expects as inputs. Values should be datatypes
-    #         convertible to Tensors, or (potentially nested) lists of
-    #         datatypes convertible to tensors.
-    #     version: str, version of the model to target.
-    # Returns:
-    #     Mapping[str: any]: dictionary of prediction results defined by the
-            model.
-    # """
     project = "aitry-294619"
     region = "us-central1"
     model = "Model_3"
     version = "v1"
-    # Create the ML Engine service object.
-    # To authenticate set the environment variable
-    # GOOGLE_APPLICATION_CREDENTIALS= '/Users/sabrageorge/Local Documents/
-    #   Capstone/full_project/aikey.json'
+
     prefix = "{}-ml".format(region) if region else "ml"
     api_endpoint = "https://{}.googleapis.com".format(prefix)
     client_options = ClientOptions(api_endpoint=api_endpoint)
@@ -143,8 +161,6 @@ def search(f):
 
     if version is not None:
         name += '/versions/{}'.format(version)
-
-    # instances = open('prediction_instances.json', 'r')
 
     response = service.projects().predict(
         name=name,
@@ -159,12 +175,6 @@ def search(f):
     pred_away = prediction_results[1]
 
     return prediction_results, pred_look, pred_away
-
-    # if response.error.message:
-        # raise Exception(
-            # '{}\nFor more info on error messages, check: '
-            # 'https://cloud.google.com/apis/design/errors'.format(
-                # response.error.message))
 
 
 if __name__ == '__main__':
